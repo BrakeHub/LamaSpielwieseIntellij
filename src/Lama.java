0e8430281e7d5ec0ba8eca5e3a84ee013ae0c5ae
KilianBeister <Kilian.Beister@gmail.com>
2026-03-17 10:19:39 +0100

    Lama um letzteSchur erweitert.

--- src/Lama.java
+++ src/Lama.java
@@ -1,8 +1,23 @@
+import java.time.LocalDate;
+
 public class Lama{
     private String tiername;
     private String tierlaut = "Möööht";
+    private LocalDate letzteSchur;
+
 
     public Lama(){
+
         System.out.println("Lama erstellt");
+        this.letzteSchur = null;
+    }
+
+    public void scheren() {
+        this.letzteSchur = LocalDate.now();
+        System.out.println(tiername + "wurde heute am " + letzteSchur + "geschoren!");
+    }
+
+    public LocalDate getLetzteSchur() {
+        return letzteSchur;
     }
 }
