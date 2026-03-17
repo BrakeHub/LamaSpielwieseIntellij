ea7eeb377f52277ba82ae752f2cc637ebcdb7acc
CFRA204 <c.frantzen@comcave.com>
2026-03-17 11:21:32 +0100

    Lama 1.0 wiederhergestellt

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main() {
       
-        Lama lama1 = new Lama("olga", "muuhhh");
-        System.out.println(lama1.getTierlaut());
-        System.out.println(lama1.getTiername());
-        lama1.ausgebenmuuhlaut();
+        Lama lama1 = new Lama();
 
-        Lama Karl = new Lama("Karl", "Proletarier-Lamas aller Länder, vereinigt euch!");
-        Lama Einstein = new Lama("Albert", "Phantasie ist wichtiger als Wissen, denn Wissen ist begrenzt.");
     }
 }
 
